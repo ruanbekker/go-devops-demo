3808454a60c1f915333c36a268c5fdad2ee4b0ee
Ruan Bekker <ruan.ru.bekker@gmail.com>
2024-08-04 23:13:59 +0200

    :wrench: fix linting

--- app/routers/router.go
+++ app/routers/router.go
@@ -8,15 +8,15 @@ import (
 func SetupRouter(userController *controllers.UserController, healthController *controllers.HealthController) *gin.Engine {
 	r := gin.Default()
 
-  // API Routes
-  r.GET("/users", userController.GetUsers)
-  r.GET("/users/:id", userController.GetUserByID)
-  r.POST("/users", userController.CreateUser)
-  r.PUT("/users/:id", userController.UpdateUser)
-  r.DELETE("/users/:id", userController.DeleteUser)
+	// API Routes
+	r.GET("/users", userController.GetUsers)
+	r.GET("/users/:id", userController.GetUserByID)
+	r.POST("/users", userController.CreateUser)
+	r.PUT("/users/:id", userController.UpdateUser)
+	r.DELETE("/users/:id", userController.DeleteUser)
 
-  // Healthcheck Routes
-  r.GET("/-/health/ready", healthController.CheckHealth)
+	// Healthcheck Routes
+	r.GET("/-/health/ready", healthController.CheckHealth)
 
 	return r
 }
